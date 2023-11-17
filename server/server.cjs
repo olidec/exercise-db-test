5c1c01708c3ebeb32382874ad526efc09e7a4437
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-11-17 13:44:04 +0100

    Minimal working example.

--- server/server.cjs
+++ server/server.cjs
@@ -1,10 +1,23 @@
 const express = require('express')
+const cors = require("cors")
 
 const app = express()
+const router = express.Router()
 
-app.get('/', (req,res) => {
-    res.send('Successful response!')
+router.get('/', (req, res) => {
     res.json({message: "Hello World"})
 })
+
+router.get("/api/test", (req, res) => {
+    res.json({ msg: "Hello", data: {
+      name: "Some Name",
+      email: "mail@nice.ch"
+    } })
+  })
+
+app.use(cors({ origin: true, credentials: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use("/", router)
 
 app.listen(3000,() => console.log('listening on port 3000'))
