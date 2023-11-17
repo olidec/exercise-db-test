cf5eb221defae1802df2a269bd9710183508e955
olidec <69003898+olidec@users.noreply.github.com>
2023-11-17 12:20:18 +0100

    erste Schritte

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 import './app.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(0)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/', {
+      method: "GET"
+    }).then((res) => res.json())
+    .then((data) => console.log(data))
+  }, []
+  )
 
   return (
     <>
