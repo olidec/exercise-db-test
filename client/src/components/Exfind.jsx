cb3cdf144e678d9d1175898d1aed931534e57f52
patrick <patrick.weber@unifr.ch>
2024-01-08 17:53:11 +0100

    uhbuzb

--- client/src/components/Exfind.jsx
+++ client/src/components/Exfind.jsx
@@ -1,31 +1,30 @@
-import { useState, useEffect } from "preact/hooks";
-import { askServer } from "../utils/connector";
-
+import { useState, useEffect } from "preact/hooks"
+import { askServer } from "../utils/connector"
 
 export default function Exfind() {
-    const [ex, setEx] = useState([{id:0}]) 
-    // useEffect(() => {
-    //     const loadAllExerciseIds = async () => {
-    //         const res = await askServer("/api/exercise-ids", "GET");
-    //         setEx(res.map(exercise => exercise.id));
-    //     };
+  const [ex, setEx] = useState([{ id: 0 }])
+  // useEffect(() => {
+  //     const loadAllExerciseIds = async () => {
+  //         const res = await askServer("/api/exercise-ids", "GET");
+  //         setEx(res.map(exercise => exercise.id));
+  //     };
 
-    //     loadAllExerciseIds();
-    // }, []);
+  //     loadAllExerciseIds();
+  // }, []);
 
-    const getEx = async () => {
-        const res = await askServer("/api/ex","GET")
-        setEx(res)
-        console.log(res[1].content)
-        document.getElementById("exercise").innerHTML = res[5].content
-        // res.map((exercise) => (
-        //     <li key={exercise.id}>{exercise.content}</li>
-        // ))
-    }
+  const getEx = async () => {
+    const res = await askServer("/api/ex", "GET")
+    setEx(res)
+    console.log(res[1].content)
+    document.getElementById("exercise").innerHTML = res[1].content
+    // res.map((exercise) => (
+    //     <li key={exercise.id}>{exercise.content}</li>
+    // ))
+  }
 
-    return (
-        <>
-            {/* <div>
+  return (
+    <>
+      {/* <div>
                 <label htmlFor="exercise">Exercise Ids</label>
                 <select>
                     {ex.map((exercise) => (
@@ -35,13 +34,10 @@ export default function Exfind() {
                     ))}
                 </select>
             </div> */}
-            <button onClick={() => getEx()}>Get Exercises</button>
-            <div>
-                <ol id="exercise">
-                    
-                </ol>
-            </div>
-
-        </>
-    )
+      <button onClick={() => getEx()}>Get Exercises</button>
+      <div>
+        <ol id="exercise"></ol>
+      </div>
+    </>
+  )
 }
